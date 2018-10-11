e2472fc21fd4606e2ebe79c75bff73c4819831b7
ALi <s.arme@ya.ru>
2018-10-11 15:11:39 +0330

    Add User list

--- UserManagementApp/UserManger.cpp
+++ UserManagementApp/UserManger.cpp
@@ -1,6 +1,11 @@
 #include "UserManger.h"
+#include<iostream>
+#include<list>
+using namespace std;
 
 std::size_t Hasher::operator()(string const & s) const {
 	std::size_t h1 = std::hash<std::string>{}(s);
 	return (h1 << 1);
 }
+
+std::list<User> userList({User("cppadmin","q!Edf@42b"), });
--- UserManagementApp/UserManger.h
+++ UserManagementApp/UserManger.h
@@ -1,5 +1,7 @@
 #pragma once
 #include<string>
+#include<list>
+#include"user.h"
 using namespace std;
 class Hasher {
 public:
