8c2ae8ed9ea20b34e9252fb4f360b35a668e98c2
ALi <s.arme@ya.ru>
2018-10-12 19:15:30 +0330

    make checkPassword public

--- UserManagementApp/user.h
+++ UserManagementApp/user.h
@@ -7,11 +7,11 @@ class User {
 private:
 	string _useranme;
 	std::size_t _paswordHash;
-	bool checkPassword(string password);
 	Profile profile;
 	User();
 public:
 	User(string useranme, string password);
 	User(string useranme, string password, string firstname, string lastname);
 	const string& getUsername();
+	bool checkPassword(string password);
 };
